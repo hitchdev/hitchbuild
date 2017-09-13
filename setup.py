6252c7beb06f2ae1529150833aff7218bc761a62
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-09-13 16:13:52 +0100

    REFACTOR : Prep for use of slugify to build folders, trigger builds, etc.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name="hitchbuild",
     url='https://hitchbuild.readthedocs.org/',
     license='AGPL',
     install_requires=[
-        'path.py', 'peewee'
+        'path.py', 'peewee', 'python-slugify',
     ],
     packages=find_packages(exclude=["docs", ]),
     package_data={},
