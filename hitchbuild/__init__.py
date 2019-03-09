f4aff9354cc5fbd8f9c867df918012c451e5b5b9
Colm O'Connor <colm.oconnor.github@gmail.com>
2019-03-09 14:04:26 +0000

    REFACTOR : Deleted crufty files.

--- hitchbuild/__init__.py
+++ hitchbuild/__init__.py
@@ -1,7 +1,4 @@
 from hitchbuild.build import HitchBuild
-from hitchbuild.build_path import BuildPath
-from hitchbuild.traits import needs
-from hitchbuild.bundle import BuildBundle
 
 
 __version__ = "DEVELOPMENT_VERSION"
